a2ba79375ae1d800d1dc76b0687eb491cb8e7135
Alex Viki <shevashokc@gmail.com>
2018-09-02 13:08:02 +0300

    Progress Bar, use setInterval

--- progressBar/js/main.js
+++ progressBar/js/main.js
@@ -1,19 +1,35 @@
-window.onload = progressBar();
+//window.onload = progressBar();
 
-function progressBar() {
-    console.log("11111");
+document.onclick = function(event) {
+    var valueProgresBar = document.getElementById('valueProgresBar');
+    var numberProgressBar = Number(valueProgresBar.value);
+
+    if (numberProgressBar === 0){
+        valueProgresBar.style.border = "1px solid red";
+    } else if (event.target.id == "btnRunProgresBar") {
+        progressBar(valueProgresBar);
+    };
+};
+
+function progressBar(time) {
     var progress = document.getElementsByTagName('progress')[0];
     var progressData = document.getElementById('progressData');
     var percent = 0;
+    time = Math.round(time * 10);
     var timeID = setInterval(function(){
             if (progress.value < 100){
-                progress.value += 1;
-                percent += 1;
-                document.getElementById('progressData').innerHTML = percent;
-                console.log(progress.value);
+                progress.value++;
+                percent++;
+                progressData.innerHTML = percent;
+                //console.log(progress.value);
             } else if (progress.value == 100) {
                 clearInterval(timeID);
+                userProgressCallBack();
             }
-        },50);
+        },time);
         console.log(progress.value);
 }
+
+function userProgressCallBack() {
+    document.querySelector(".hiden-block").style.display = "block";
+};
