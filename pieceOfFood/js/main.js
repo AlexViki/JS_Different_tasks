3871aa2e513524eaa4aa2bae79ad6fe591992aee
Alex Viki <shevashokc@gmail.com>
2018-09-16 21:36:22 +0300

    still try to solve this task

--- pieceOfFood/js/main.js
+++ pieceOfFood/js/main.js
@@ -1,31 +1,38 @@
-let days = 21;
-let period = 3;
-let eatDay = 0;
-let totalCalories = 0;
-let tmp = 0;
-let su = 0; // Saturday
-let sa = 0; // Sunday
-function takeCalories () {
+function intakeCalories () {
+    let days = 28;
+    let period = 3;
+    let totalCalories = 0;
+    let tmp = 1;
+    let su = 1; // Saturday
+    let sa = 1; // Sunday
+        
     for (let i = 1; i <= days; i++) {
+        if(tmp === period) {
+            if(su === 6) {
+                totalCalories = totalCalories + 200;
+                tmp = 0;
+                su = 1;
+                sa = 2;
+                console.log(i + ":su weekEnd(200) - "  + totalCalories);
+            } else if (sa === 7) {
+                totalCalories = totalCalories + 200;
+                tmp = 0;
+                su = 1;
+                sa = 2;
+                console.log(i + ":sa weekEnd(200) - "  + totalCalories);
+            } else if (tmp === period) {
+                totalCalories = totalCalories + 300;
+                tmp = 0;
+                console.log(i + ": week(300) - "  + totalCalories);
+            }
+        } 
+        if (su == 6) su = 0;
+        if (sa == 7) sa = 0;
         tmp++;
         su++;
         sa++;
-        if(tmp === period && su === 6 ){
-            totalCalories = totalCalories + 200;
-            tmp = 0;
-            su = 0;
-            sa = 1;
-        } else if (tmp === period & sa === 7) {
-            totalCalories = totalCalories + 200;
-            tmp = 0;
-            sa = 0;
-        } else if (tmp === period) {
-            totalCalories = totalCalories + 300;
-            tmp = 0;
-        }
     }
-    //console.log(eatDay);
     console.log(totalCalories);
-};
+}
 
-takeCalories();
+intakeCalories();
