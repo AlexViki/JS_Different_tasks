aac10c7d344421e57941aa7ba06ae40136f751ab
Alex Viki <shevashokc@gmail.com>
2018-09-17 15:19:25 +0300

    created new input fields, add some easy style CSS

--- dosageOfTablets/js/main.js
+++ dosageOfTablets/js/main.js
@@ -4,30 +4,34 @@ function calculatePeriod() {
     let period = document.getElementById('period-number').value;
     let days = document.getElementById('period-days').value;
     let result = document.getElementById('result');
+    let workdayPills = document.getElementById('workday-number-pills').value
+    let weekEndPills = document.getElementById('weekend-number').value
     let text = 'You need: ';
-    let totalCalories = 0;
+    let totalPills = 0;
     let tmp = 1;
     let weekEnd = 1;
     period = +period;
+    workdayPills = +workdayPills;
+    weekEndPills = +weekEndPills;
 
     for (let i = 1; i <= days; i++) {
         if(tmp === period) {
             if(weekEnd === 6 || weekEnd === 7) {
-                totalCalories = totalCalories + 2;
+                totalPills = totalPills + weekEndPills;
                 tmp = 0;
-                console.log(i + ":su weekEnd(200) - "  + totalCalories);
+                console.log(i + ": weekEnd(" + weekEndPills + ") = "  + totalPills);
 
             } else if (tmp === period) {
-                totalCalories = totalCalories + 1;
+                totalPills = totalPills + workdayPills;
                 tmp = 0;
-                console.log(i + ": week(300) - "  + totalCalories);
+                console.log(i + ": week(" + workdayPills + ") = "  + totalPills);
             }
         } 
         if (weekEnd === 7) weekEnd = 0;
         tmp++;
         weekEnd++;
     }
-    console.log(totalCalories);
-    result.innerHTML += text + totalCalories + ' pills.' + '<br>';
+    console.log(totalPills);
+    result.innerHTML += text + totalPills + ' pills.' + '<br>';
 }
 //calculatePeriod();
